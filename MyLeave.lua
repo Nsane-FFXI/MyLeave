77aa0cce4ee954e47ee9c952e96134e8c8a82a68
Nsane-FFXI <EnsaneDRK@gmail.com>
2025-09-05 01:37:19 -0400

    Update MyLeave.lua

--- MyLeave.lua
+++ MyLeave.lua
@@ -1,119 +1,129 @@
 _addon.name     = 'MyLeave'
 _addon.author   = 'Nsane'
-_addon.version  = '23.11.9'
+_addon.version  = '2025.09.05'
 _addon.commands = {'myleave', 'leave', 'ml'}
 
+require('sets')
+local res = require('resources')
 
-require('resources')
-res = require('resources')
+-- Bag 0 = inventory
+local INVENTORY_BAG = 0
 
-
-function leave_zone()
-	local items = windower.ffxi.get_items(0)
-	local zone = windower.ffxi.get_info().zone
-
-	-- Assaults
-	if T{55, 56, 63, 66, 69}:contains (zone) then
-		windower.send_command('Input /item "Azouph Fireflies" <me>')
-		windower.send_command('Input /item "Bhaflau Fireflies" <me>')
-		windower.send_command('Input /item "Dvucca Fireflies" <me>')
-		windower.send_command('Input /item "Reef Fireflies" <me>')
-		windower.send_command('Input /item "Zhayolm Fireflies" <me>')
-
-
-	-- Peach
-	elseif T{60}:contains (zone) then
-		windower.send_command('Input /item "Cutter Fireflies" <me>')
-
-
-	-- Salvage
-	elseif T{73, 74, 75, 76}:contains (zone) then
-	        for index, item in pairs(items) do
-                if type(item) == 'table' and T{5365,5366,5367,5368,5369,5370,5371,5372,5373,5374,5375,5376,5377,5378,5379,5380,5381,5382,5383,5384}:contains (item.id) and item.status == 0 then -- Drop's all Cells at once.
-                    windower.ffxi.drop_item(index, item.count)
-                end
-            end
-		windower.send_command('Input /item "A. Rem. Fireflies" <me>')
-		windower.send_command('Input /item "B. Rem. Fireflies" <me>')
-		windower.send_command('Input /item "S. Rem. Fireflies" <me>')
-		windower.send_command('Input /item "Z. Rem. Fireflies" <me>')
-		
-		
-	-- Nyzul Battlefields, added this incase future battlefields
-	-- elseif T{77}:contains (zone) then
-		-- windower.send_command('Input /item "Un. Ruins Fireflies " <me>')
-
-
-	-- Einhejar
-	elseif T{78}:contains (zone) then
-            for index, item in pairs(items) do
-                if type(item) == 'table' and item.id == 5414 and item.status == 0 then -- Drop's all Glowing Lamps at once.
-                    windower.ffxi.drop_item(index, item.count)
-                end
-            end
-
-
-	-- Meebles & MMM
-	elseif T{129}:contains (zone) then
-		windower.send_command('Input /item "Hiatus Whistle" <me>')
-		windower.send_command('Input /item "Maze Compass" <me>')
-
-
-	-- Sortie & Vagary
-	elseif T{133, 189, 275}:contains (zone) then
-		windower.send_command('Input /item "Obsid. Wing" <me>')
-		windower.send_command('Input /item "Ontic Extremity" <me>')
-
-
-	-- SR
-	elseif T{259}:contains (zone) then
-		windower.send_command('Input /item "Arena Fireflies" <me>')
-
-
-	-- Delve & Incursion
-	elseif T{264, 271}:contains (zone) then
-		windower.send_command('Input /item "Ontic Extremity" <me>')
-
-
-	-- Omen
-	elseif T{292}:contains (zone) then
-		windower.send_command('Input /item "Reisenjima Cage" <me>')
-
-
-	-- Dynamis(D)
-	elseif T{294, 295, 296, 297}:contains (zone) then
-		windower.send_command('Input /item "Black Hourglass" <me>')
-
-
-	-- HTMB & Odyssey
-	elseif T{279, 298}:contains (zone) then
-		windower.send_command('Input /item "Moglophone" <me>')
-		windower.send_command('Input /item "Moglophone II" <me>')
-		windower.send_command('Input /item "V. Con. Shard" <me>')
-
-	end
-
+-- Fast lookup of inventory by item id
+local function get_inventory()
+    return windower.ffxi.get_items(INVENTORY_BAG) or {}
 end
 
+local function inventory_has_item_name(wanted_name)
+    if not wanted_name then return false end
+    local items = get_inventory()
+    for index, slot in pairs(items) do
+        if type(slot) == 'table' and slot.id and slot.status == 0 then
+            local r = res.items[slot.id]
+            if r and r.en and r.en:lower() == wanted_name:lower() then
+                return true
+            end
+        end
+    end
+    return false
+end
 
-windower.register_event('addon command',function(...)
+local function drop_all_by_ids(id_set)
+    local items = get_inventory()
+    for index, slot in pairs(items) do
+        if type(slot) == 'table' and slot.id and slot.status == 0 and id_set:contains(slot.id) then
+            windower.ffxi.drop_item(index, slot.count)
+        end
+    end
+end
+
+local function use_if_owned(names)
+    for _, name in ipairs(names) do
+        if inventory_has_item_name(name) then
+            windower.send_command(('input /item "%s" <me>'):format(name))
+        end
+    end
+end
+
+local SALVAGE_CELL_IDS = T{
+    5365,5366,5367,5368,5369,5370,5371,5372,5373,5374,
+    5375,5376,5377,5378,5379,5380,5381,5382,5383,5384
+}
+
+local function leave_zone()
+    local zone = windower.ffxi.get_info().zone
+
+    -- Assaults
+    if T{55,56,63,66,69}:contains(zone) then
+        use_if_owned{
+            'Azouph Fireflies','Bhaflau Fireflies','Dvucca Fireflies',
+            'Reef Fireflies','Zhayolm Fireflies'
+        }
+
+    -- Periqia (Peach in your note)
+    elseif T{60}:contains(zone) then
+        use_if_owned{'Cutter Fireflies'}
+
+    -- Salvage
+    elseif T{73,74,75,76}:contains(zone) then
+        drop_all_by_ids(SALVAGE_CELL_IDS)
+        use_if_owned{
+            'A. Rem. Fireflies','B. Rem. Fireflies','S. Rem. Fireflies','Z. Rem. Fireflies'
+        }
+
+    -- Nyzul Isle (kept commented in original)
+    -- elseif T{77}:contains(zone) then
+    --     use_if_owned{'Un. Ruins Fireflies'}
+
+    -- Einherjar
+    elseif T{78}:contains(zone) then
+        -- Drop all Glowing Lamps (5414)
+        drop_all_by_ids(T{5414})
+
+    -- Meebles & MMM
+    elseif T{129}:contains(zone) then
+        use_if_owned{'Hiatus Whistle','Maze Compass'}
+
+    -- Sortie & Vagary
+    elseif T{133,189,275}:contains(zone) then
+        use_if_owned{'Obsid. Wing','Ontic Extremity'}
+
+    -- Skirmish (SR)
+    elseif T{259}:contains(zone) then
+        use_if_owned{'Arena Fireflies'}
+
+    -- Delve & Incursion
+    elseif T{264,271}:contains(zone) then
+        use_if_owned{'Ontic Extremity'}
+
+    -- Omen
+    elseif T{292}:contains(zone) then
+        use_if_owned{'Reisenjima Cage'}
+
+    -- Dynamis [D]
+    elseif T{294,295,296,297}:contains(zone) then
+        use_if_owned{'Black Hourglass'}
+
+    -- HTMB & Odyssey
+    elseif T{279,298}:contains(zone) then
+        use_if_owned{'Moglophone','Moglophone II','V. Con. Shard'}
+    end
+end
+
+windower.register_event('addon command', function(...)
     local args = T{...}
-    local cmd = args[1]
+    local cmd = args[1] and args[1]:lower()
 
     if cmd == 'all' then
         windower.chat.input('//myleave')
         windower.send_ipc_message('myleave')
-        else
-            leave_zone()
-        end
-
+    else
+        leave_zone()
+    end
 end)
 
-
-windower.register_event('ipc message',function (msg)
-
+windower.register_event('ipc message', function(msg)
     if msg == 'myleave' then
         windower.chat.input('//myleave')
     end
-
 end)
